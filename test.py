6c90aab1ca5537df24f31e5785ca0bcb97d60a21
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-06-02 09:43:19 +0000

    okkkkkk

--- test.py
+++ test.py
@@ -8,7 +8,8 @@ async def main():
     async with connect("ws://localhost:8080/gateway") as ws:
         print(loads(zlib.decompress(await ws.recv())))
         await ws.send(zlib.compress(b'{"type": "identify", "token": "yHogyE4a7nYy3kQXgt3cpLoz12HgCKDw"}'))
-        print(loads(zlib.decompress(await ws.recv())))
+        while True:
+            print(loads(zlib.decompress(await ws.recv())))
 
 
 asyncio.run(main())
