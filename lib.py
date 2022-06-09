9845d51b79d45cd3113a81b81c0c724ac617b91a
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-09 21:15:19 +0900

    Update lib.py

--- lib.py
+++ lib.py
@@ -6,13 +6,20 @@ db = TinyDB('db.json')
 table = db.table("token")
 user = Query()
 
+def json(data: dict=None, *, message: str=None,
+         status: int=200):
+    success = True
+    if status != 200:
+        success = False
+    return response.json({"success": success, "status": status, "message": message, "data": data})
+
 def authorized():
     def decorator(f):
         @wraps(f)
         async def decorated_function(request, *args, **kwargs):
             s = table.search(user.token == request.token)
             if len(s) == 0:
-                return response.json({"success": False}, status=401)
+                return json(message="Authorized failed", status=401)
             else:
                 return await f(request, s[0]["user"], *args, **kwargs)
         return decorated_function
